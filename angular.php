f79187bb70b12b9f787643686efe48ad6dc1e145
Chihab Otmani <chihab@gmail.com>
2014-07-20 10:28:18 +0000

    Add an entry in module table on install

--- angular.php
+++ angular.php
@@ -19,37 +19,44 @@ class Angular extends Module
 
     public function install()
     {
-        Db::getInstance()->execute('
+        $tables =
+            Db::getInstance()->execute('
                 CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . $this->name . '` (
                     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
                     name VARCHAR(32) NOT NULL,
                     value LONGTEXT NOT NULL
-                ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8;');
-        return $this->installTab("Admin".get_class(), $this->l('Angular Module'), "AdminParentStats") && parent::install();
+                )') &&
+            Db::getInstance()->execute('
+                INSERT INTO `' . _DB_PREFIX_ . $this->name . '` VALUES(
+                    NULL,
+                    "appearance",
+                    "{}"
+                )');
+        return $tables && $this->installTab($this->l('Angular Module'), "AdminParentStats") && parent::install();
     }
 
     public function uninstall()
     {
         Db::getInstance()->execute('DROP TABLE IF EXISTS ' . _DB_PREFIX_ . $this->name);
-        return $this->uninstallTab("Admin".get_class()) && parent::uninstall();
+        return $this->uninstallTab() && parent::uninstall();
     }
 
-    private function installTab($class, $name, $parent)
+    private function installTab($name, $parent)
     {
         $nameLang = array();
         foreach (Language::getLanguages() as $language)
             $nameLang[$language['id_lang']] = $name;
         $tab = new Tab();
         $tab->name = $nameLang;
-        $tab->class_name = $class;
+        $tab->class_name = "Admin" . get_class();
         $tab->module = $this->name;
         $tab->id_parent = Tab::getIdFromClassName($parent);
         return $tab->save();
     }
 
-    private function uninstallTab($class)
+    private function uninstallTab()
     {
-        $idTab = Tab::getIdFromClassName($class);
+        $idTab = Tab::getIdFromClassName("Admin" . get_class());
         if ($idTab != 0) {
             $tab = new Tab($idTab);
             return $tab->delete();
